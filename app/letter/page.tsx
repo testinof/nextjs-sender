9b07ac736f5b52eb82479ae5039675d8c27230ab
testinof <testinofg@hotmail.com>
2024-11-04 18:28:00 +0100

    delete pages

--- app/letter/page.tsx
+++
@@ -1,5 +0,0 @@
-import EmailLink from "@/components/EmailLink";
-
-export const DocumentReviewEmail = () => <EmailLink />;
-
-export default DocumentReviewEmail;
--- app/react/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import EmailTemplate from "@/components/EmailTemplate";
-
-export default function page() {
-  return <EmailTemplate />;
-}
